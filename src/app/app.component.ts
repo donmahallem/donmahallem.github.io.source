55f4e32bc4badaaef952a0e5275e6b85265a09ff
donmahallem <donmahallem@users.noreply.github.com>
2019-06-04 23:51:52 +0200

    add github api service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GithubApiService } from './services';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DonMahallem';
+  constructor(private test: GithubApiService) {
+    test.getRepos('DonMahallem').subscribe(console.log, console.error);
+  }
 }
