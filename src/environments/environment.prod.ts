a546afcf7a51f6c6601059c187b6831a1a5d6430
donmahallem <donmahallem@users.noreply.github.com>
2019-06-05 23:32:48 +0200

    lowercase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   github: {
-    username: 'DonMahallem'
+    username: 'donmahallem'
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   github: {
-    username: 'DonMahallem'
+    username: 'donmahallem'
   }
 };
 
