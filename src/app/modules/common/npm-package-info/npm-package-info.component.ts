e75dbe4a00f262c7b1bd74e8913e00bceddb30e6
donmahallem <donmahallem@users.noreply.github.com>
2019-06-08 14:30:52 +0200

    ammending

--- src/app/modules/common/npm-package-info/npm-package-info.component.ts
+++ src/app/modules/common/npm-package-info/npm-package-info.component.ts
@@ -1,16 +1,24 @@
-import { Component, AfterViewInit, OnDestroy, NgZone, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
+import { Component, AfterViewInit, OnDestroy, NgZone, ChangeDetectorRef, ChangeDetectionStrategy, Input } from '@angular/core';
 import { Repository } from 'src/app/modal';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { MatTreeFlattener, MatTreeFlatDataSource } from '@angular/material';
 import { FlatTreeControl } from '@angular/cdk/tree';
+import { NpmPackageService } from './npm-package.service';
 
 @Component({
     selector: 'app-npm-package-info',
     templateUrl: './npm-package-info.component.html',
     styleUrls: ['./npm-package-info.component.scss'],
+    providers: [NpmPackageService]
 })
 export class NpmPackageInfoComponent {
+    constructor(private packageService: NpmPackageService) {
 
+    }
+    @Input()
+    public set packageUrl(url: string) {
+        this.packageService.setPackageUrl(url);
+    }
 }
---
+++ src/app/modules/common/npm-package-info/npm-package.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class NpmPackageService {
+
+    public setPackageUrl(url: string): void {
+
+    }
+}
