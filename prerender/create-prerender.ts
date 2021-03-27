b1001f35a988ecdc496b27c6e4e3c42b1d83d0fd
DonMahallem <donmahallem@users.noreply.github.com>
2021-03-27 14:30:51 +0100

    chore: save

--- prerender/create-prerender.ts
+++ prerender/create-prerender.ts
@@ -5,7 +5,6 @@ const username: string = 'DonMahallem';
 const pageSize: number = 100;
 const BEARER: string = (process.env.TOKEN) ? `Bearer ${process.env.TOKEN}` : '';
 const getPage = async (page: number): Promise<any[]> => {
-    console.log(`Repo Page ${page} by ${username}`);
     return fetch(`https://api.github.com/users/${username}/repos?per_page=${pageSize}&page=${page}`, {
         headers: {
             'User-Agent': 'Prerender DonMahallem repo',
@@ -15,7 +14,7 @@ const getPage = async (page: number): Promise<any[]> => {
     })
         .then((res: Response): any => {
             if (res.status === 429) {
-                throw new Error('Github Rate Limited');
+                return Promise.reject(new Error('Github Rate Limited'));
             }
             return res.json();
         });
@@ -23,8 +22,11 @@ const getPage = async (page: number): Promise<any[]> => {
 }
 const create = async (): Promise<void> => {
     const data: any[] = [];
-    for (let i = 0; i < 4; i++) {
-        const resp: any[] = await getPage(i);
+    for (let page = 0; page < 4; page++) {
+        console.group(`Repo Page ${page} by ${username}`);
+        const resp: any[] = await getPage(page);
+        console.log(`Got ${resp.length} items`);
+        console.groupEnd();
         data.push(resp);
         if (resp.length < 100) {
             break;
