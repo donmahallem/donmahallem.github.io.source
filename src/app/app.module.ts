924e536d5997037a7d34af009063fca26a8acea8
ò_ó <donmahallem@users.noreply.github.com>
2021-07-29 13:49:00 +0200

    feat: option to add api token for prerendering (#747)
    * feat: use api token interceptor
    
    * ci(github): add prerender testing
    
    * ci(github): add API_TOKEN env
    
    * ci(github): add API_TOKEN env

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@
  * Source https://github.com/donmahallem/donmahallem.github.io.source
  */
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
@@ -11,7 +11,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home.component';
 import { NotFoundComponent } from './not-found.component';
-import { GithubApiService } from './services';
+import { AuthInterceptor, GithubApiService } from './services';
 
 @NgModule({
   bootstrap: [AppComponent],
@@ -29,6 +29,7 @@ import { GithubApiService } from './services';
   ],
   providers: [
     GithubApiService,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ],
 })
 export class AppModule { }
