9a298f2f63d9be3fad04463d47d13399aa155217
ò_ó <donmahallem@users.noreply.github.com>
2022-03-05 19:17:32 +0100

    feat: add repository prerendering (#1057)
    * feat: add prerendering
    
    * fix: add repos gitignore
    
    * feat: generate all repo bundles
    
    * fix: add python requirement file
    
    * ci(github): add repository loading
    
    * ci(github): add installation step
    
    * feat: output description
    
    * fix: site generation

---
+++ prerender.py
@@ -0,0 +1,45 @@
+import requests
+import os
+import time
+import codecs
+import json
+
+username = 'DonMahallem'
+pageSize = 25
+BEARER = None
+if 'TOKEN' in os.environ:
+    pageSize = 100
+    BEARER = f"Bearer {os.environ['TOKEN']}"
+
+if BEARER != '':
+    print('Bearer is set')
+
+def getPage(username,pageSize,page):
+    params = {
+        "User-Agent":'Prerender DonMahallem repo',
+        'content-type': 'application/json',
+    }
+    if BEARER is not None:
+        params['authorization'] = BEARER
+    resp =requests.get(f"https://api.github.com/users/{username}/repos?per_page={pageSize}&page={page}", params=params)
+
+    return resp.json() 
+os.makedirs("./content/repos/", exist_ok=True)
+for page in range(0,1000):
+    print(f"Loading page: {page}")
+    pageContent = getPage(username=username,pageSize=pageSize,page=page)
+    print(f"Contained: {len(pageContent)}")
+    for repo in pageContent:
+        pageData = dict()
+        pageData['source']=repo
+        pageData['title']=repo['name']
+        pageData['date']=repo['created_at']
+        pageData['lastmod']=repo['updated_at']
+        pageData['categories']=["repository"]
+        pageData['description']=["description"]
+        with codecs.open("./content/repos/"+ repo["name"]+".md",'w') as f:
+            f.write("---\n"+json.dumps(pageData)+"\n---\n")
+    if len(pageContent) < pageSize:
+        break
+
+    time.sleep(1)
